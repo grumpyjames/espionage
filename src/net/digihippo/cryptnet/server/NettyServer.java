97d4b8b0a8b0c510049d1eec769ca438046888b1
James Byatt <grumpyjames@gmail.com>
2020-12-30 12:41:00 +0000

    Reshape the server so it might be runnable from a test harness as well as main

--- src/net/digihippo/cryptnet/server/NettyServer.java
+++ src/net/digihippo/cryptnet/server/NettyServer.java
@@ -15,63 +15,58 @@ import net.digihippo.cryptnet.roadmap.LatLn;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NettyServer {
-    private final int port;
-
-    public NettyServer(int port) {
-        this.port = port;
+    interface Stoppable {
+        void stop();
     }
 
-    public void run() throws Exception {
-        final AtomicInteger clientCounter = new AtomicInteger(0);
+    public static Stoppable runServer(final int port) throws Exception {
+        AtomicInteger clientCounter = new AtomicInteger(0);
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         int onlyOneThread = 1;
         EventLoopGroup workerGroup = new NioEventLoopGroup(onlyOneThread);
-        try {
-            ServerBootstrap publicBootstrap = new ServerBootstrap();
-            publicBootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch)
-                        {
-                            int clientId = clientCounter.getAndIncrement();
-                            System.out.println("Client " + clientId + " connected");
-                            ch.pipeline()
-                                    .addLast(
-                                            new LengthFieldBasedFrameDecoder(1024, 0, 2),
-                                            new DiscardServerHandler(clientId, new ProtocolV1()));
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-            Bootstrap timeBootstrap = new Bootstrap();
-            timeBootstrap.group(workerGroup)
-                    .channel(NioDatagramChannel.class)
-                    .handler(new ChannelInitializer<>()
+        ServerBootstrap publicBootstrap = new ServerBootstrap();
+        publicBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch)
                     {
-                        @Override
-                        public void initChannel(Channel ch)
-                        {
-                            int clientId = clientCounter.getAndIncrement();
-                            System.out.println("Client " + clientId + " connected");
-                            ch.pipeline().addLast(new UdpHandler());
-                        }
-                    });
+                        int clientId = clientCounter.getAndIncrement();
+                        System.out.println("Client " + clientId + " connected");
+                        ch.pipeline()
+                                .addLast(
+                                        new LengthFieldBasedFrameDecoder(1024, 0, 2),
+                                        new DiscardServerHandler(clientId, new ProtocolV1()));
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+        Bootstrap timeBootstrap = new Bootstrap();
+        timeBootstrap.group(workerGroup)
+                .channel(NioDatagramChannel.class)
+                .handler(new ChannelInitializer<>()
+                {
+                    @Override
+                    public void initChannel(Channel ch)
+                    {
+                        int clientId = clientCounter.getAndIncrement();
+                        System.out.println("Client " + clientId + " connected");
+                        ch.pipeline().addLast(new UdpHandler());
+                    }
+                });
 
 
-            ChannelFuture publicFuture = publicBootstrap.bind(port).sync();
-            ChannelFuture timeFuture = timeBootstrap.bind("127.0.0.1", port + 1).sync();
+        publicBootstrap.bind(port).sync();
+        timeBootstrap.bind("127.0.0.1", port + 1).sync();
 
-            // Wait until the server socket is closed.
-            // In this example, this does not happen, but you can do that to gracefully
-            // shut down your server.
-            publicFuture.channel().closeFuture().sync();
-            timeFuture.channel().closeFuture().sync();
-        } finally {
+        return () ->
+        {
+            System.out.println("Shutting down");
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
-        }
+        };
     }
 
     public static void main(String[] args) throws Exception {
@@ -80,7 +75,8 @@ public class NettyServer {
             port = Integer.parseInt(args[0]);
         }
 
-        new NettyServer(port).run();
+        Stoppable stoppable = NettyServer.runServer(port);
+        Runtime.getRuntime().addShutdownHook(new Thread(stoppable::stop));
     }
 
     private static class UdpHandler extends ChannelInboundHandlerAdapter {
